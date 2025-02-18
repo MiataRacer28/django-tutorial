f132ca7c087101294cae8fcfecaa0fca954e5631
[MiataRacer28] <[littl28@rocketmail.com]>
2025-02-18 11:03:31 -0500

    Mobile Responsiveness

--- base/urls.py
+++ base/urls.py
@@ -15,6 +15,10 @@ urlpatterns = [
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
     path('update-user/', views.updateUser, name="update-user"),
     path('delete-mesage/<str:pk>/', views.deleteMessage, name="delete-message"),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
+
+
 
 
 ]
